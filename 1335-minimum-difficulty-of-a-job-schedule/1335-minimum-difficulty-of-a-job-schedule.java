c62280a7e1e2ecd0c055ec7c9d175ab6ccb677fc
adi_walture <adityawalture9298@gmail.com>
2023-12-29 22:35:12 +0530

    Time: 18 ms (26.52%), Space: 44 MB (5.26%) - LeetHub

---
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minDifficulty(int[] jobDifficulty, int d) {
+        int n = jobDifficulty.length;
+        
+        if(n < d)
+            return -1;
+        int[][] t = new int[n + 1][d + 1];
+        
+        for(int i=0; i < n; i++){
+            t[i][1] = Arrays.stream(Arrays.copyOfRange(jobDifficulty, i, n)).max().orElse(Integer.MIN_VALUE);
+        }
+        
+        for(int days = 2; days <= d; days++){
+            for(int i = 0; i <= n - days; i++){
+                int maxDifficulty = Integer.MIN_VALUE;
+                int result = Integer.MAX_VALUE;
+                
+                for(int j = i; j <= n-days; j++){
+                    maxDifficulty = Math.max(maxDifficulty, jobDifficulty[j]);
+                    result = Math.min(result, maxDifficulty + t[j + 1][days - 1]);
+                }
+                t[i][days] = result;
+            }
+        }
+        return t[0][d];
+    }
+}
